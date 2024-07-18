86fff79fe86ef0a231fcadf4f045f2afee3996f7
Argal <arvin.bonggal@gmail.com>
2024-07-18 15:04:44 +0800

    Building the layout

--- src/App.js
+++ src/App.js
@@ -1,1 +1,34 @@
+export default function app() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
 
+function Logo() {
+  return <h1>😎 Far Away 😎</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      😀 You have X items on you list, and you already packed X (X%)
+    </footer>
+  );
+}
